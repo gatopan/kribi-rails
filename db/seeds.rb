26727657c572d88f9ef39d0d190f356f9e842459
Ivan Rodriguez <gatopan@icloud.com>
2016-06-01 04:04:10 -0700

    added sample users, sample initial private home chart and fixed a export bug with target day

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,32 +11,112 @@
 
 [
   {
-    first_name: 'approver',
-    last_name: 'approver',
-    email: 'approver@kribi.com',
-    email_confirmation: 'approver@kribi.com',
+    first_name: 'Approver',
+    last_name: '1',
+    email: 'approver-1@kribi.com',
+    email_confirmation: 'approver-1@kribi.com',
     password: 'password',
-    biography: 'approver',
+    biography: 'hello there',
     role: :APPROVER,
     avatar: File.open("./test/fixtures/sample_avatar.png")
   },
   {
-    first_name: 'reviewer',
-    last_name: 'reviewer',
-    email: 'reviewer@kribi.com',
-    email_confirmation: 'reviewer@kribi.com',
+    first_name: 'Approver',
+    last_name: '2',
+    email: 'approver-2@kribi.com',
+    email_confirmation: 'approver-2@kribi.com',
     password: 'password',
-    biography: 'reviewer',
+    biography: 'hello there',
+    role: :APPROVER,
+    avatar: File.open("./test/fixtures/sample_avatar.png")
+  },
+  {
+    first_name: 'Approver',
+    last_name: '3',
+    email: 'approver-3@kribi.com',
+    email_confirmation: 'approver-3@kribi.com',
+    password: 'password',
+    biography: 'hello there',
+    role: :APPROVER,
+    avatar: File.open("./test/fixtures/sample_avatar.png")
+  },
+  {
+    first_name: 'Reviewer',
+    last_name: '1',
+    email: 'reviewer-1@kribi.com',
+    email_confirmation: 'reviewer-1@kribi.com',
+    password: 'password',
+    biography: 'hello there',
     role: :REVIEWER,
     avatar: File.open("./test/fixtures/sample_avatar.png")
   },
   {
-    first_name: 'clerk',
-    last_name: 'clerk',
-    email: 'clerk@kribi.com',
-    email_confirmation: 'clerk@kribi.com',
+    first_name: 'Reviewer',
+    last_name: '2',
+    email: 'reviewer-2@kribi.com',
+    email_confirmation: 'reviewer-2@kribi.com',
     password: 'password',
-    biography: 'clerk',
+    biography: 'hello there',
+    role: :REVIEWER,
+    avatar: File.open("./test/fixtures/sample_avatar.png")
+  },
+  {
+    first_name: 'Reviewer',
+    last_name: '3',
+    email: 'reviewer-3@kribi.com',
+    email_confirmation: 'reviewer-3@kribi.com',
+    password: 'password',
+    biography: 'hello there',
+    role: :REVIEWER,
+    avatar: File.open("./test/fixtures/sample_avatar.png")
+  },
+  {
+    first_name: 'Inputter',
+    last_name: '1',
+    email: 'inputter-1@kribi.com',
+    email_confirmation: 'inputter-1@kribi.com',
+    password: 'password',
+    biography: 'hello there',
+    role: :CLERK,
+    avatar: File.open("./test/fixtures/sample_avatar.png")
+  },
+  {
+    first_name: 'Inputter',
+    last_name: '2',
+    email: 'inputter-2@kribi.com',
+    email_confirmation: 'inputter-2@kribi.com',
+    password: 'password',
+    biography: 'hello there',
+    role: :CLERK,
+    avatar: File.open("./test/fixtures/sample_avatar.png")
+  },
+  {
+    first_name: 'Inputter',
+    last_name: '3',
+    email: 'inputter-3@kribi.com',
+    email_confirmation: 'inputter-3@kribi.com',
+    password: 'password',
+    biography: 'hello there',
+    role: :CLERK,
+    avatar: File.open("./test/fixtures/sample_avatar.png")
+  },
+  {
+    first_name: 'Inputter',
+    last_name: '4',
+    email: 'inputter-4@kribi.com',
+    email_confirmation: 'inputter-4@kribi.com',
+    password: 'password',
+    biography: 'hello there',
+    role: :CLERK,
+    avatar: File.open("./test/fixtures/sample_avatar.png")
+  },
+  {
+    first_name: 'Inputter',
+    last_name: '5',
+    email: 'inputter-5@kribi.com',
+    email_confirmation: 'inputter-5@kribi.com',
+    password: 'password',
+    biography: 'hello there',
     role: :CLERK,
     avatar: File.open("./test/fixtures/sample_avatar.png")
   }
